8798dea67a02a073f116ab0d39e1ee0c0b775d58
Phillip Hsu <philliphsu@outlook.com>
2016-09-27 03:51:33 -0700

    Set theme result from onBackPressed

--- app/src/main/java/com/philliphsu/clock2/settings/SettingsActivity.java
+++ app/src/main/java/com/philliphsu/clock2/settings/SettingsActivity.java
@@ -1,6 +1,11 @@
 package com.philliphsu.clock2.settings;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.MenuItem;
 
 import com.philliphsu.clock2.BaseActivity;
 import com.philliphsu.clock2.R;
@@ -11,9 +16,14 @@ import com.philliphsu.clock2.R;
 public class SettingsActivity extends BaseActivity {
     public static final String EXTRA_THEME_CHANGED = "com.philliphsu.clock2.settings.extra.THEME_CHANGED";
 
+    private String mInitialTheme;
+    private SharedPreferences mPrefs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        mInitialTheme = getSelectedTheme();
     }
 
     @Override
@@ -29,5 +39,32 @@ public class SettingsActivity extends BaseActivity {
     @Override
     protected boolean isDisplayShowTitleEnabled() {
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                setThemeResult(getSelectedTheme());
+                return false; // Don't capture, proceed as usual
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        setThemeResult(getSelectedTheme());
+        super.onBackPressed();
+    }
+
+    private String getSelectedTheme() {
+        return mPrefs.getString(getString(R.string.key_theme), "");
+    }
+
+    private void setThemeResult(String selectedTheme) {
+        Intent result = new Intent();
+        result.putExtra(EXTRA_THEME_CHANGED, !selectedTheme.equals(mInitialTheme));
+        setResult(Activity.RESULT_OK, result);
     }
 }
