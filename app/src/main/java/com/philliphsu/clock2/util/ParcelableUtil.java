44e1f5faf41ff280fb4596b585eb318d63975770
Phillip Hsu <philliphsu@outlook.com>
2017-03-01 17:32:42 -0800

    Update copyrights in files.

--- app/src/main/java/com/philliphsu/clock2/util/ParcelableUtil.java
+++ app/src/main/java/com/philliphsu/clock2/util/ParcelableUtil.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2017 Phillip Hsu
+ *
+ * This file is part of ClockPlus.
+ *
+ * ClockPlus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ClockPlus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ClockPlus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.philliphsu.clock2.util;
 
 import android.os.Parcel;
